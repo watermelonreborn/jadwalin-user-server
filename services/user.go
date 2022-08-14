c860cd1d089f9bb2346e214deedbbee12575f4ca
James Frederix Rolianto <james.frederix@ui.ac.id>
2022-08-14 15:40:35 +0700

    fixing service

--- services/user.go
+++ services/user.go
@@ -1,19 +1,12 @@
 package services
 
 import (
-	"context"
 	"fmt"
 	"jadwalin/models"
-
-	"go.mongodb.org/mongo-driver/bson"
 )
 
-func CreateUser(userRequest models.UserRequest) {
+func CreateUser(userRequest models.UserDiscordRequest) {
 	// TODO: create logic to write user to database and save user's server
-	MongoClient.StartSession()
-	db := MongoClient.Database("jadwalin").Collection("users")
-	doc := bson.D{{"user_id", "abc-321"}, {"server_id", "abc-123"}, {"token", "3131kshfakhfsa"}}
-	db.InsertOne(context.TODO(), doc)
 	fmt.Println(userRequest)
 }
 
