00534213da388a2da2c578c8eb6eb7c0ab67c4ca
James Frederix Rolianto <james.frederix@ui.ac.id>
2022-08-14 15:57:40 +0700

    add column in user models

--- models/user.go
+++ models/user.go
@@ -4,6 +4,7 @@ import "time"
 
 type User struct {
 	ID            string    `json:"user_id"`
+	Name          string    `json:"name"`
 	GoogleAccount string    `json:"google_account"`
 	CreatedAt     time.Time `json:"created_at"`
 	UpdatedAt     time.Time `json:"updated_at"`
